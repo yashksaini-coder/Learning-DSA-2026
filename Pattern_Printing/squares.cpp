98efea240780578beea5c14bd702aa880ab7492a
Yash Saini <yashksaini89@gmail.com>
2026-09-18 21:14:48 +0530

    Did the square pattern printing

---
+++ Pattern_Printing/squares.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cout << "*";
+    for (int j = 1; j <= n; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
