5fb88996747bbe488ad35512007bb66c9190520b
Yash Saini <yashksaini89@gmail.com>
2026-09-19 13:19:58 +0530

    initialized a vector with diff value types an int, and a double to see the compilation error

---
+++ Pascal_triangle/compilation_error.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vector<int> data{1, 2.3};
+  for (auto number : data) {
+    cout << number << " ";
+  }
+  return 0;
+}
