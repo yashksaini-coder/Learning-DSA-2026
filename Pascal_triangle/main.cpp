f1fc227c82037ca55f4c27e22a2fd44accbfa526
Yash Saini <yashksaini89@gmail.com>
2026-09-19 13:19:30 +0530

    main entrypoint for generating pascal triangle using a vector first

---
+++ Pascal_triangle/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void generate_trianlge() {
+  vector<int> data{1};
+
+  for (auto number : data) {
+    cout << number << ' ';
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  generate_trianlge();
+
+  return 0;
+}
